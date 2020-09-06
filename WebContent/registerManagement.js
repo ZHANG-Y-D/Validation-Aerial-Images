bca43adf47cbbc5d2acba3a42cbf1bd8d39babbf
LeoETZ <424355505@qq.com>
2020-09-06 20:30:14 +0200

    register function complected

--- WebContent/registerManagement.js
+++ WebContent/registerManagement.js
@@ -2,49 +2,140 @@
  * Login management
  */
 
-(function() { // avoid variables ending up in the global scope
+(function (newChild) { // avoid variables ending up in the global scope
 
-  document.getElementById("submitbutton").addEventListener('click', (e) => {
+  const roleSelection = document.getElementById("RoleSelection");
+  const registerForm = document.getElementById("RegisterForm");
+  const managerButton = document.getElementById("managerButton");
+  const workerButton = document.getElementById("workerButton");
+  const insertToFieldset = document.getElementById("forLavoratore");
+  var errorMessage = document.getElementById("errormessage");
+  const roleTag  = document.getElementsByName("role");
 
-    var pwd = document.getElementsByName("pwd")[0].value;
-    var confrimPwd = document.getElementsByName("confirmpwd")[0].value;
-    if (pwd !== confrimPwd){
-      document.getElementById("errormessage").textContent =
-          "Notice from client side: Password and confirm password must be the same.";
-      return;
-    }
-    var email = document.getElementsByName("email")[0].value;
-    // let re = new RegExp("^(\\S+)\\@(\\S+).(\\S+)$");
-    var re = new RegExp("^(\\S+)\\@(\\S+).(\\S+)$");
-    if(!re.test(email)){
-      document.getElementById("errormessage").textContent =
-          "Notice from client side: The syntactic of the email address is not valid.";
-      return;
-    }
 
-    var form = e.target.closest("form");
-    if (form.checkValidity()) {
-      makeCall("POST", 'Register', e.target.closest("form"),
-        function(req) {
-          if (req.readyState === XMLHttpRequest.DONE) {
-            var message = req.responseText;
-            switch (req.status) {
-              case 200:
-                window.location.href = "loginPage.html";
-                break;
-              case 406: // data non acceptable
-                document.getElementById("errormessage").textContent = message;
-                break;
-              case 500: // server error
-            	document.getElementById("errormessage").textContent = message;
-                break;
-            }
-          }
+
+  const pageOrchestrator = new PageOrchestrator();
+  const role = new Role();
+  const register = new Register();
+  roleSelection.style.visibility = "hidden";
+  registerForm.style.visibility = "hidden";
+
+
+
+  window.addEventListener("load", () => {
+        pageOrchestrator.start(); // initialize the components
+        pageOrchestrator.refresh(); // display initial content
+  }, false);
+
+  function PageOrchestrator(){
+
+        this.start = function(){
+              role.show();
         }
-      );
-    } else {
-    	 form.reportValidity();
-    }
-  });
+
+        this.refresh = function(){
+
+        }
+
+  }
+
+  function Role(){
+        var self = this;
+        this.show = function () {
+              roleSelection.style.visibility = "visible";
+              managerButton.addEventListener("click", (e) => {
+                self.update("manager");
+              }, false);
+              workerButton.addEventListener("click", (e) => {
+                self.update("worker");
+              }, false);
+        }
+
+
+        this.update = function (roleName){
+            roleSelection.style.visibility = "hidden";
+            if(roleName === "manager"){
+                  roleTag[0].value = "Manager";
+            }else {
+                roleTag[0].value = "Lavoratore";
+
+                let lavoratoreLevel = document.createElement("label");
+                lavoratoreLevel.textContent = "LavoratoreLevel:";
+                insertToFieldset.appendChild(lavoratoreLevel);
+                let select = document.createElement("select");
+                select.name = "lavoratoreLevel";
+                let optionBassa = document.createElement("option");
+                optionBassa.value = "basso";
+                optionBassa.textContent = "Basso";
+                let optionMedia = document.createElement("option");
+                optionMedia.value = "medio";
+                optionMedia.textContent = "Medio";
+                let optionAlto = document.createElement("option");
+                optionAlto.value = "alto";
+                optionAlto.textContent = "Alto";
+                select.appendChild(optionBassa);
+                select.appendChild(optionMedia);
+                select.appendChild(optionAlto);
+                insertToFieldset.appendChild(select);
+
+                let imageLabel = document.createElement("label");
+                imageLabel.textContent = "Image:";
+                insertToFieldset.appendChild(imageLabel);
+                let image = document.createElement("input");
+                image.type = 'file';
+                image.name = 'image';
+                imageLabel.appendChild(image);
+            }
+            register.show();
+        }
+  }
+
+
+  function Register(){
+      var self = this;
+      this.show = function () {
+          registerForm.style.visibility = "visible";
+          document.getElementById("submitbutton").addEventListener('click', (e) => {
+
+              let pwd = document.getElementsByName("pwd").value;
+              let confirmPwd = document.getElementsByName("confirmed").value;
+              if (pwd !== confirmPwd) {
+                  errorMessage.textContent =
+                        "Notice from client side: Password and confirm password must be the same.";
+                  return;
+              }
+              let email = document.getElementsByName("email")[0].value;
+              let re = new RegExp("^(\\S+)\\@(\\S+).(\\S+)$");
+              if (!re.test(email)) {
+                  errorMessage.textContent =
+                        "Notice from client side: The syntactic of the email address is not valid.";
+                  return;
+              }
+
+              var form = e.target.closest("form");
+
+              if (form.checkValidity()) {
+                  makeCall("POST", 'Register', e.target.closest("form"),function(req) {
+                      if (req.readyState === XMLHttpRequest.DONE) {
+                          var message = req.responseText;
+                          if (req.status === 200) {
+                            window.location.href = "loginPage.html";
+                          } else {
+                            errorMessage.innerHTML = "";
+                            errorMessage.textContent = message;
+                          }
+                      }
+                  });
+              } else {
+                form.reportValidity();
+              }
+
+          })
+      }
+
+      this.update = function (){
+
+      }
+  }
 
 })();
--- src/it/polimi/tiw/projects/controllers/Register.java
+++ src/it/polimi/tiw/projects/controllers/Register.java
@@ -1,6 +1,7 @@
 package it.polimi.tiw.projects.controllers;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -33,56 +34,70 @@ public class Register extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws IOException {
+
+
+		UserDAO userDao = new UserDAO(connection);
+		String resultString = null;
 		// obtain and escape params
 		String usrn = null;
 		String pwd = null;
-		String confirmpwd = null;
+		String confirmed = null;
 		String email = null;
-		
-		usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
-		pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
-		confirmpwd = StringEscapeUtils.escapeJava(request.getParameter("confirmpwd"));
-		email = StringEscapeUtils.escapeJava(request.getParameter("email"));
-		
-		if (usrn == null || pwd == null || confirmpwd == null || email == null
-				|| usrn.isEmpty() || pwd.isEmpty() || confirmpwd.isEmpty() || email.isEmpty()) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			response.getWriter().println("Credentials must be not null");
-			return;
-		}
-		
-		if (!pwd.equals(confirmpwd)) {
-			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-			response.getWriter().println("Password and confirm password must be the same.");
-			return;
-		}
-		
-		// insert into database
-		UserDAO userDao = new UserDAO(connection);
-		String resultString = null;
+		String role = null;
+		String lavoratoreLevel = null;
+		InputStream lavoratoreFoto = null;
+
 		try {
-			resultString = userDao.insertForRegister(usrn, pwd, email);
+			usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
+			confirmed = StringEscapeUtils.escapeJava(request.getParameter("confirmed"));
+			email = StringEscapeUtils.escapeJava(request.getParameter("email"));
+			role = StringEscapeUtils.escapeJava(request.getParameter("role"));
+
+
+			if (usrn == null || pwd == null || confirmed == null || email == null || role == null ||
+					usrn.isEmpty() || pwd.isEmpty() || confirmed.isEmpty() || email.isEmpty() || role.isEmpty()) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().println("Credentials must be not null");
+				return;
+			}
+
+			if (role.equals("Lavoratore")){
+				lavoratoreLevel = StringEscapeUtils.escapeJava(request.getParameter("lavoratoreLevel"));
+				lavoratoreFoto = request.getPart("image").getInputStream();
+				if (lavoratoreLevel == null || lavoratoreLevel.isEmpty() || lavoratoreFoto == null){
+					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+					response.getWriter().println("Credentials must be not null");
+					return;
+				}
+			}
+
+			if (!pwd.equals(confirmed)) {
+				response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+				response.getWriter().println("Password and confirm password must be the same.");
+				return;
+			}
+
+			resultString = userDao.insertForRegister(usrn, pwd, email, role, lavoratoreLevel,lavoratoreFoto);
 			if (resultString.contains("OK")) {
 				response.setStatus(HttpServletResponse.SC_OK);
 			}else {
 				response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-				if (resultString.contains("Duplicate")) { 
-					// Duplicate entry. The user name has been registered.
-					response.getWriter().println("The user name has been registered.");
-				}else if(resultString.contains("mailchck")){
-					// Check constraint 'mailchck' is violated,
-					// So the syntactic of the email address is not valid.
-					response.getWriter().println("The syntactic of the email address is not valid.");
-				}else {
-					response.getWriter().println(resultString);
-				}
+				response.getWriter().println(resultString);
 			}
+
 		} catch (SQLException e) {
-			
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			response.getWriter().println("Internal server error, retry later. "+e.getMessage());
-			return;
+			try {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				response.getWriter().println("Internal server error, retry later. "+e.getMessage());
+			} catch (IOException ignore){
+
+			}
+		} catch (IOException ignore){
+
+		} catch (ServletException e) {
+			e.printStackTrace();
 		}
 	}
 
